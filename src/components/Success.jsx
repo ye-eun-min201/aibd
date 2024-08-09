1e7c367b7e2abf487cb9e04e6c03a7bbf2b278ab
ye-eun-min201 <remnantyeeun@gmail.com>
2024-08-09 10:39:52 +0900

    인기 유튜버 카테고리 수정

--- src/components/Success.jsx
+++ src/components/Success.jsx
@@ -20,89 +20,59 @@ const Success = () => {
     ],
     "푸드/먹방": [
       {
-        name: "승비니 Seungbini",
-        url: "https://www.youtube.com/watch?v=EogRAvEce0M",
+        name: "[햄지]Hamzy",
+        url: "https://www.youtube.com/watch?v=uNcsp0TUFOA",
       },
       {
-        name: "CuRe 구래",
-        url: "https://www.youtube.com/watch?v=e_ANyCbbXus",
+        name: "Jane ASMR 제인",
+        url: "https://www.youtube.com/watch?v=ZwXE7A-KLjw",
       },
       {
-        name: "KIMPRO STUDIO",
-        url: "https://www.youtube.com/watch?v=t0jVwannoIg",
+        name: "Hongyu ASMR 홍유",
+        url: "https://www.youtube.com/watch?v=mzN_x5hWtlU",
       },
     ],
     엔터테인먼트: [
       {
+        name: "Mnet K-POP",
+        url: "https://www.youtube.com/watch?v=qkLMPihvKaQ",
+      },
+      {
+        name: "KBS WORLD TV",
+        url: "https://www.youtube.com/watch?v=_ZNF4NDCrTM",
+      },
+      {
         name: "Stray Kids",
-        url: "https://www.youtube.com/watch?v=BYabYht7k9Q",
-      },
-      {
-        name: "조동아리",
-        url: "https://www.youtube.com/watch?v=p2t7RVCG-ic",
-      },
-      {
-        name: "Workman",
-        description: "다양한 직업 체험을 다루는 콘텐츠로 인기를 얻고 있습니다.",
-        url: "https://www.youtube.com/watch?v=job_experience",
+        url: "https://www.youtube.com/watch?v=k8Y6ZTjmCXs",
       },
     ],
-    게임: [
+
+    여행: [
       {
-        name: "먹방왕 (Mukbang King)",
-        description:
-          "다양한 음식을 맛보고 먹는 먹방 콘텐츠로 인기를 끌고 있습니다.",
-        url: "https://www.youtube.com/watch?v=mukbang",
+        name: "빠니보틀 Pani Bottle",
+        url: "https://www.youtube.com/watch?v=uaBHe5P4JF8",
       },
       {
-        name: "홈가드닝 (Home Gardening)",
-        description:
-          "가정에서 할 수 있는 원예와 정원 가꾸기 콘텐츠로 사랑받고 있습니다.",
-        url: "https://www.youtube.com/watch?v=gardening",
+        name: "곽튜브",
+        url: "https://www.youtube.com/watch?v=RSv0K4hQyV8",
       },
       {
-        name: "HEOPOP",
-        description:
-          "다양한 실험과 일상 브이로그를 다루며 어린이와 청소년에게 인기가 많습니다.",
-        url: "https://www.youtube.com/watch?v=experiment_fail",
-      },
-    ],
-    패션: [
-      {
-        name: "전원생활 (Country Life)",
-        description:
-          "전원에서의 삶과 농사, 자급자족 콘텐츠로 인기를 얻고 있습니다.",
-        url: "https://www.youtube.com/watch?v=country_life",
-      },
-      {
-        name: "건강정보 (Health Info)",
-        description:
-          "다양한 건강 관련 정보를 제공하며, 건강 관리에 관심이 많은 사람들에게 유익합니다.",
-        url: "https://www.youtube.com/watch?v=health_info",
-      },
-      {
-        name: "유라준의 문화발전소 (Yurajun's Cultural Development)",
-        description: "역사와 문화에 대한 깊이 있는 설명을 제공합니다.",
-        url: "https://www.youtube.com/watch?v=culture_talk",
+        name: "jannahkorea 잔나코리아",
+        url: "https://www.youtube.com/watch?v=St-eM7JZdqE",
       },
     ],
     뷰티: [
       {
-        name: "할담비 (Hal-Dambi)",
-        description:
-          "60대 유튜버로 다양한 춤과 노래를 통해 젊은이들과 소통합니다.",
-        url: "https://www.youtube.com/watch?v=dance_cover",
+        name: "PONY Syndrome",
+        url: "https://www.youtube.com/watch?v=x9EF-r4LMUI",
       },
       {
-        name: "박막례 할머니 (Park Makrye)",
-        description:
-          "70대 유튜버로 요리와 여행 브이로그를 통해 많은 사랑을 받고 있습니다.",
-        url: "https://www.youtube.com/watch?v=travel_vlog",
+        name: "화장하는청담언니",
+        url: "https://www.youtube.com/watch?v=Dukox0oFb3k",
       },
       {
-        name: "동네 할머니 (Grandma of the Neighborhood)",
-        description: "일상 속의 소소한 이야기를 통해 따뜻한 감동을 전합니다.",
-        url: "https://www.youtube.com/watch?v=daily_life",
+        name: "RISABAE",
+        url: "https://www.youtube.com/watch?v=TSjVsxsQTes",
       },
     ],
   };
